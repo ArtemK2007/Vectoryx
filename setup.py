50c4e87ec50f86ff77ac64d72b5152d604034a17
ArtemK2007 <artemkryyachko2007@gmail.com>
2025-07-05 13:01:05 +0200

    gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Vectoryx',
-    version='0.1.0',
+    version='0.1.1',
     author='Artem Kryachko',
     author_email='artemkryachko2007@email.com',
     description='A simple vector operations library',
