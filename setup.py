0ffa55c857abe6166c2754780b862154be4e2fef
ArtemK2007 <artemkryyachko2007@gmail.com>
2025-07-05 13:03:51 +0200

    0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Vectoryx',
-    version='0.1.1',
+    version='0.1.2',
     author='Artem Kryachko',
     author_email='artemkryachko2007@email.com',
     description='A simple vector operations library',
